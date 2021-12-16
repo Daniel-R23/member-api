bd7e0490c7b4d172ddd70d0da588c4039c090043
Daniel Rodrigues <53978097+Daniel-R23@users.noreply.github.com>
2021-12-16 19:57:14 -0300

    Add generics at MembroRepository

--- src/main/java/com/daniel/memberapi/repository/MembroRepository.java
+++ src/main/java/com/daniel/memberapi/repository/MembroRepository.java
@@ -1,6 +1,7 @@
 package com.daniel.memberapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.daniel.memberapi.entity.Membro;
 
-public interface MembroRepository extends JpaRepository {
+public interface MembroRepository extends JpaRepository<Membro, Long> {
 }
